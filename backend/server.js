fdd6e929cab3b3d1c3393b0fdf251ffb0688c533
unknown <logplayviews@gmail.com>
2025-04-30 21:32:00 -0300

    Feat: Implementation of a server, and a simple page for a frontend

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import shortid from "shortid";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const mongoUri = process.env.MONGO_URI;
+
+await mongoose.connect(mongoUri);
+
+const urlSchema = new mongoose.Schema({
+    originalUrl: {type: String, required: true},
+    shortUrl: {type: String, required: true, unique: true}
+})
+
+const Url = mongoose.model("Url",urlSchema);
+
+//ZOD for validatin in a more serius programs
+//Error handling middleware
+app.post("/api/shorten", async (req,res) => {
+    const {originalUrl} = req.body;
+    const shortUrl = shortid.generate();
+    const newUrl = new Url({originalUrl, shortUrl});
+    await newUrl.save();
+    res.status(201).json({originalUrl, shortUrl});
+});
+
+app.get("/:shortUrl", async (req,res)=>{
+    const {shortUrl} = req.params;
+    const url = await Url.findOne({shortUrl});
+
+    if(url){
+        return res.redirect(url.originalUrl);
+    }else{
+        return res.status(404).json("Url not found");
+    }
+});
+
+app.listen(3000, () => console.log(`Server running on port 3000`));
